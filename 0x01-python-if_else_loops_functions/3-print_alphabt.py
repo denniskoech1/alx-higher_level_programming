5c959c1ee5b24190b1dfca9862ff0547064fc9a5
denniskoech1 <denniskipkoech19@gmail.com>
2024-02-25 19:35:58 +0000

    New updates

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) != 'e' and chr(i) != 'q':
-        print('{:c}'.format(i), end='')
+for char in range(97, 123):
+    if char == 101 or char == 113:
+        continue
+    print('{:c}'.format(char), end="")
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for num in range(0, 99):
-    print('{:02d}, '.format(num), end='')
-print('99')
+for int in range(0, 100):
+    if int != 99:
+        print('{:02d},'.format(int), end=' ')
+    else:
+        print('{}'.format(int))
