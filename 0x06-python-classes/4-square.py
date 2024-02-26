41fef356f31cadac141a176b776700fd7802aad0
denniskoech1 <denniskipkoech19@gmail.com>
2024-02-26 20:21:10 +0000

    Few changes for adv tasks

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,52 +1,32 @@
 #!/usr/bin/python3
-""" Module 4-square: class Square """
+
+"""Define a class Square."""
 
 
-class Square():
-    """
-        Square: defines a square.
-        Attributes:
-            size (int): size of square.
-        Method:
-                __init__ : init of size attribute for each instance.
-    """
+class Square:
+    """Represent a square."""
 
     def __init__(self, size=0):
+        """Initialize a new square.
 
-        """ Initialization of attributes for instances
-            Args:
-                size (int): size of the square.
+        Args:
+            size (int): The size of the new square.
         """
-        if not (isinstance(size, int)):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
-        """ getter function for private attribute size.
-            Returns:
-                size.
-        """
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """ setter function for private attribute size.
-            Args:
-                value: value to be set.
-            Returns:
-                nothing
-        """
-        if not (isinstance(value, int)):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        """
-             area of the square.
-        """
-        return self.__size * self.
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
